41142dcd2ce28ddf5930ebb9c7eb491c1c2fbc96
wlillisdev <williamlillis100@gmail.com>
2023-03-25 18:46:14 +0000

    onclick event added

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,19 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = ["Cork", "Kerry", "Dublin", "Galway"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>no items found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
