9d4782ad1204b22ee4c943d1bc766b13c3bf1e1a
wlillisdev <williamlillis100@gmail.com>
2023-03-25 17:47:58 +0000

    added a list

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  const items = ["Cork", "Kerry", "Dublin", "Galway"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
